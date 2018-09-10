a67360dde67c117371e7dda701ce11830798880e
Tanure Uyovbievbo <tanureuyo.21772@redlandsschools.net>
2018-09-10 15:26:20 +0000

    age

---
+++ age.py
@@ -0,0 +1,7 @@
+age = int(input('How old are you?'))
+print ('You are '+age+'.')
+if age >= 18
+    print ('Make sure you vote!')
+if age < 16
+    print ("Ahhhh. You can't vote quite yet")
+
---
+++ number.py
@@ -0,0 +1,4 @@
+number = int(input("Enter a number: "))
+print (number)
+print ('Let me add 6 to that.')
+print (6+number)
